904b78971098b3bc9bde41f2fddd8f410e329a70
Alex <alexjlaberge@gmail.com>
2015-02-19 17:19:54 -0500

    Starting implementing probabilities. Need to implement variable size possibleHands

--- main.cpp
+++ main.cpp
@@ -2,34 +2,66 @@
 #include <vector>
 #include <cstdlib>
 #include <string>
+#include <map>
+#include <algorithm>
 
 using namespace std;
+
+vector<string> cards;
 
 class Player
 {
 public:
 	int cardNum;
 	string name;
+	vector<vector<string>> possibleHands;
+	map<string, double> probabilities;
+	vector<string> hand;
+
 	Player(int numCards, string nameP)
 	{
 		name = nameP;
 		cardNum = numCards;
-		generateHands(cardNum);
 	}
-	void generateHands(int cardNum)
+
+	void generateHands(int numCards, int i, vector<string> hand)
+	{
+		
+	}
+
+	void removeHands(string a, string b, string c)
 	{
 
 	}
+
 	void updateProbabilities()
 	{
+		int count;
+		vector<string> c;
+		for(int i = 0; i < cards.size(); i++)
+		{
+			count = 0;
+			for(int j = 0; j < possibleHands.size(); j++)
+			{
+				c = possibleHands[j];
+				if(find(c.begin(), c.end(), cards[i]) != c.end())
+				{
+					count++;
+				}
+			}
+			probabilities[cards[i]] = (double)count / possibleHands.size();
+		}
+	}
 
+	double getProbability(string guess)
+	{
+		return probabilities[guess];
 	}
 };
 
 int main()
 {
 	//Guaranteed cards
-	vector<string> cards;
 	cards = {"Rope", "Wrench", "Pistol", "Candlestick", "Pipe", "Dagger",
 			 "Ballroom", "Kitchen", "DiningRoom", "BilliardRoom",
 			 "Lounge", "Library", "Study", "Conservatory", "Hall"};
@@ -40,20 +72,65 @@ int main()
 	cout << "Which characters are playing?\n";
 	cin >> players;
 	numPlayers = players.size();
+	vector<Player*> myPlayers;
+	string name;
+	string numCards;
 	for(int i = 0; i < players.size(); i++)
 	{
 		if(players[i] == 'R')
+		{
+			cout << "Scarlets Name: ";
+			cin >> name;
+			cout << "Scarlets # of Cards: ";
+			cin >> numCards;
+			myPlayers.push_back(new Player(atoi(numCards.c_str()), name));
 			cards.push_back("Scarlet");
+		}
 		else if(players[i] == 'Y')
+		{
+			cout << "Mustards Name: ";
+			cin >> name;
+			cout << "Mustards # of Cards: ";
+			cin >> numCards;
+			myPlayers.push_back(new Player(atoi(numCards.c_str()), name));
 			cards.push_back("Mustard");
+		}
 		else if(players[i] == 'W')
+		{
+			cout << "Whites Name: ";
+			cin >> name;
+			cout << "Whites # of Cards: ";
+			cin >> numCards;
+			myPlayers.push_back(new Player(atoi(numCards.c_str()), name));
 			cards.push_back("White");
+		}
 		else if(players[i] == 'G')
+		{
+			cout << "Greens Name: ";
+			cin >> name;
+			cout << "Greens # of Cards: ";
+			cin >> numCards;
+			myPlayers.push_back(new Player(atoi(numCards.c_str()), name));
 			cards.push_back("Green");
+		}
 		else if(players[i] == 'B')
+		{
+			cout << "Peacocks Name: ";
+			cin >> name;
+			cout << "Peacocks # of Cards: ";
+			cin >> numCards;
+			myPlayers.push_back(new Player(atoi(numCards.c_str()), name));
 			cards.push_back("Peacock");
+		}
 		else if(players[i] == 'P')
+		{
+			cout << "Plums Name: ";
+			cin >> name;
+			cout << "Plums # of Cards: ";
+			cin >> numCards;
+			myPlayers.push_back(new Player(atoi(numCards.c_str()), name));
 			cards.push_back("Plum");	
+		}
 	}
 
 	//Combination Generator
